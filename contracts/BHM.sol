5bb804b1b4f0fc91909db3dc5dd7a2d9ea5530d7
SANGHYUN <SANGHYUN@14.52.141.148>
2018-02-22 14:30:57 +0900

    delete unused code

--- contracts/BHM.sol
+++ contracts/BHM.sol
@@ -5,14 +5,7 @@ import './internal_library/common/Blocked.sol';
 
 
 contract BHM is MiniMeToken, Blocked {
-  bool public sudoEnabled = true;
-  
   mapping (address => bool) public admin;
-
-  modifier onlySudoEnabled() {
-    require(sudoEnabled);
-    _;
-  }
 
   modifier onlyNotBlocked(address _addr) {
     require(!blocked[_addr]);
@@ -24,19 +17,16 @@ contract BHM is MiniMeToken, Blocked {
     _;
   }
 
-  event SudoEnabled(bool _sudoEnabled);
-
   function BHM(address _tokenFactory) MiniMeToken(
     _tokenFactory,
-    0x0,                  // no parent token
-    0,                    // no snapshot block number from parent
+    0x0,                  // parent token
+    0,                    // snapshot block number from parent
     "BHOM",        // Token name
     18,                   // Decimals
     "BHM",                // Symbol
     true                 // Enable transfers
   ) public {}
 
-  /*For token control*/
   function transfer(address _to, uint256 _amount) public onlyNotBlocked(msg.sender) returns (bool success) {
     return super.transfer(_to, _amount);
   }
@@ -45,32 +35,27 @@ contract BHM is MiniMeToken, Blocked {
     return super.transferFrom(_from, _to, _amount);
   }
 
-  function generateTokens(address _owner, uint _amount) public onlyController onlySudoEnabled returns (bool) {
+  function generateTokens(address _owner, uint _amount) public onlyController  returns (bool) {
     return super.generateTokens(_owner, _amount);
   }
 
-  function destroyTokens(address _owner, uint _amount) public onlyController onlySudoEnabled returns (bool) {
+  function destroyTokens(address _owner, uint _amount) public onlyController  returns (bool) {
     return super.destroyTokens(_owner, _amount);
   }
 
-  function blockAddress(address _addr) public onlyController onlySudoEnabled {
+  function blockAddress(address _addr) public onlyController {
     super.blockAddress(_addr);
   }
 
-  function unblockAddress(address _addr) public onlyController onlySudoEnabled {
+  function unblockAddress(address _addr) public onlyController {
     super.unblockAddress(_addr);
-  }
-
-  function enableSudo(bool _sudoEnabled) public onlyController {
-    sudoEnabled = _sudoEnabled;
-    SudoEnabled(_sudoEnabled);
   }
   
   function enableTransfers(bool _transfersEnabled) public onlyController {
     super.enableTransfers(_transfersEnabled);
   }
 
-  function generateTokensByList(address[] _owners, uint[] _amounts) public onlyController onlySudoEnabled returns (bool) {
+  function generateTokensByList(address[] _owners, uint[] _amounts) public onlyController  returns (bool) {
     require(_owners.length == _amounts.length);
 
     for(uint i = 0; i < _owners.length; i++) {
@@ -79,11 +64,7 @@ contract BHM is MiniMeToken, Blocked {
 
     return true;
   }
-  
-  /**
-   * @dev set new admin as admin of KYC contract
-   * @param _addr address The address to set as admin of KYC contract
-   */
+
   function setAdmin(address _addr, bool _value)
     public
     onlyController
