d09bd447298dc9a70e4a0d0e335086206c73a488
bhomlab <36035146+bhomlab@users.noreply.github.com>
2018-03-21 14:54:47 +0900

    change mispell

--- contracts/external_library/minime/MiniMeToken.sol
+++ contracts/external_library/minime/MiniMeToken.sol
@@ -666,7 +666,7 @@ contract MiniMeToken is Controlled {
 		updateValueAtNow(balances[_to], previousBalanceTo + _amount);
 		
         // An event to make the deposit easy to find on the blockchain
-        WithDeposit(_from, _to, _amount);        
+        WithdrawDeposit(_from, _to, _amount);        
 	}
     
 	
