9564899dc26c97addf51f0b0be37c618f3e546a4
bhomlab <36035146+bhomlab@users.noreply.github.com>
2018-03-26 18:21:27 +0900

    set Admin

--- scripts/admin/setAdmin.js
+++ scripts/admin/setAdmin.js
@@ -43,7 +43,7 @@ function setAdmin(_seedOwner, _bhmAddress, _addr, _value) {
     to: _bhmAddress,
     data: makeDataField(_addr, _value),
     gas: 1000000,
-    gasPrice: 30e9,
+    gasPrice: 25e9,
   });
 }
 
