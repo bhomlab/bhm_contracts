254b073541064a9aa9fab84d8cb66b3ee6fc3fe0
bhomlab <36035146+bhomlab@users.noreply.github.com>
2018-03-29 11:29:15 +0900

    initial commit

---
+++ contracts/internal_library/User.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.19;
+
+contract User {
+  mapping (address => string) email;
+
+  event RegisterEmail(address _addr, string _email);
+
+  function registerEmail(string _email) public {
+    email[msg.sender] = _email;
+
+    RegisterEmail(msg.sender);
+  }
+
+}
