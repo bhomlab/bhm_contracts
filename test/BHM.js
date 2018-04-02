96c13096c3e50ab57a9aedf90d3ec03ed2f2a1eb
TomKim <tgkim@bhom.io>
2018-04-02 21:06:55 +0900

    test bhm.js

--- test/BHM.js
+++ test/BHM.js
@@ -48,164 +48,220 @@ contract("BHM", async ([ owner, other, beneficiary1, beneficiary2, ...accounts ]
 
       await token.generateTokens(beneficiary1, amount, { from: owner })
         .should.be.fulfilled;
-
+      // await token.generateTokens(beneficiary2, amount, { from: owner })
+      //   .should.be.fulfilled;
       const balance2 = await token.balanceOf(beneficiary1);
 
-      console.log(balance2);
+      console.log("beneficiary1: " + balance2);
+      //console.log("balance1: " + balance1 + " balance2: " + balance2);
 
       assert.equal(balance2 - amount, balance1);
     });
   });
 
-  //  describe("#2 block", async () => {
-  //     it("2-1 only controller can block account", async () => {
-  //       await token.blockAddress(beneficiary2, {
-  //        from: beneficiary1,
-  //       }).should.be.rejectedWith(EVMThrow, "revert");
+  describe("#2 block", async () => {
+    it("2-1 only controller can block account", async () => {
+      await token.blockAddress(beneficiary2, {
+        from: beneficiary1,
+      }).should.be.rejected;
+
+      await token.blockAddress(beneficiary2, {
+        from: owner,
+      }).should.be.fulfilled;
+    });
+
+    it("2-2 blocked user should not transfer tokens", async () => {
+       await token.transfer(beneficiary1, amount, {
+         from: beneficiary2,
+       }).should.be.rejected;
+    });
+
+    it("2-3 only controller can unblock account", async () => {
+       await token.unblockAddress(beneficiary2, {
+         from: beneficiary1,
+       }).should.be.rejected;
+
+       await token.unblockAddress(beneficiary2, {
+         from: owner,
+       }).should.be.fulfilled;
+    });
+
+    // 안되는거 같음 trnasfer
+    // it("2-4 unblocked user should transfer tokens", async () => {
+    //    // await token.generateTokens(beneficiary2, amount, { from: owner })
+    //    //  .should.be.fulfilled;
+    //    await token.unblockAddress(beneficiary2, {
+    //      from: owner,
+    //    }).should.be.fulfilled;
+    //    console.log("test");
+    //    await token.transfer(beneficiary1, amount, {
+    //      from: beneficiary2,
+    //    }).should.be.fulfilled;
+    //    // console.log("test11");
+    //    // const other1 = await token.balanceOf(beneficiary2);
+    //    // console.log("other1 :" + other1);
+    //    // (await token.balanceOf(beneficiary2))
+    //    //   .assert.equal(0, "transfer success");
+    //    // (await token.balanceOf(beneficiary1))
+    //    //   .assert.equal(amount, amount);
+    //  });
+  });
+
+  // describe("#3 destroyTokens", async () => {
+  //    it("3-1 only controller should destroyTokens", async () => {
+  //      const balance1 = await token.balanceOf(beneficiary1);
   //
-  //       await token.blockAddress(beneficiary2, {
-  //         from: owner,
-  //       }).should.be.fulfilled;
-  //     });
+  //      console.log("beneficiary1: " + balance1);
+  //      //토큰파쾨 0
+  //      await token.destroyTokens(beneficiary1, amount, { from: owner, }).should.be.fulfilled;
   //
-  //   // it("2-2 blocked user should not transfer tokens", async () => {
-  //   //    await token.transfer(beneficiary1, amount, {
-  //   //      from: beneficiary2,
-  //   //    }).assert.throws(EVMThrow, "revert");
-  //   // });
-  //   //
-  //   // it("2-3 only controller can unblock account", async () => {
-  //   //    await token.unblockAddress(beneficiary2, {
-  //   //      from: beneficiary1,
-  //   //    }).should.be.throws(EVMThrow, "revert");
-  //   //
-  //   //    await token.unblockAddress(beneficiary2, {
-  //   //      from: owner,
-  //   //    }).assert.ok();
-  //   // });
-  //   //
-  //   // it("2-4 unblocked user should transfer tokens", async () => {
-  //   //    await token.transfer(beneficiary1, amount, {
-  //   //      from: beneficiary2,
-  //   //    }).should.be.fulfilled;
-  //   //
-  //   //    (await token.balanceOf(beneficiary2))
-  //   //      .assert.equal(0, "transfer success");
-  //   //    (await token.balanceOf(beneficiary1))
-  //   //      .assert.equal(amount, amount);
-  //   //  });
+  //      const balance2 = await token.balanceOf(beneficiary1);
+  //      console.log("beneficiary1: " + balance2);
+  //      //일치 확인
+  //      assert.equal(balance2, 0);
+  //   });
   // });
 
-  describe("#3 destroyTokens", async () => {
-    it("3-1 only controller should destroyTokens", async () => {
-      const balance1 = await token.balanceOf(beneficiary1);
-
-      console.log(balance1);
-      //토큰파쾨 0
-      await token.destroyTokens(beneficiary1, amount, { from: owner }).should.be.fulfilled;
-
-      const balance2 = await token.balanceOf(beneficiary1);
-      console.log(balance2);
-      //잔고 0 확인
-      assert.equal(0, balance2);
-    });
-  });
-
-  describe("#4 deposit", async () => {
-    // it("4-1 setDeposit", async () => {
-    //   const balance1 = await token.balanceOf(beneficiary1);
-    //   console.log(balance1);
-    //   await token.generateTokens(beneficiary1, amount, { from: owner }).should.be.fulfilled;
-    //   const balance2 = await token.balanceOF(beneficiary1);
-    //   await token.setDeposit(beneficiary1, owner, amount).should.be.fulfilled;
-    //   const balance3 = await token.balanceOF(beneficiary1);
-    // });
-    // it("4-2 updateDepositValueAtNow", async () => {
-    //    await token.updateDepositValueAtNow(checkpoints, depositValue, claimerDepositValue, to {
-    //
-    //    }).should.be.fulfilled;
-    //  });
-    // it("4-3 depositBalanceOfAt", async () => {
-    //   await token.depositBalanceOfAt(owner, blockNumber {
-    //
-    //   }).should.be.fulfilled;
-    // });
-    // it("4-4 getDepositValueAt", async () => {
-    //   await token.getDepositValueAt(checkpoints, block {
-    //
-    //   }).should.be.fulfiled
-    // });
-    // it("4-5 claimerBalanceAt", async () => {
-    //   await token.claimerBalanceAt(owner, blockNumber, to{
-    //
-    //   }).should.be.fulfiled
-    // });
-    // it("4-6 getClaimerValueAt", async () => {
-    //   await token.getClaimerValueAt(checkpoints, block, to{
-    //
-    //   }).should.be.fulfiled
-    // });
-    // it("4-7 withdrawDeposit", async () => {
-    //    await token.withdrawDeposit (from, to, amount {
-    //
-    //    }).should.be.fulfiled
-    //  });
-  });
+  // describe("#4 deposit", async () => {
+  //   it("4-1 setDeposit", async () => {
+  //     const balance1 = await token.balanceOf(beneficiary1);
+  //     console.log(balance1);
+  //     // await token.generateTokens(beneficiary1, amount, { from: owner }).should.be.fulfilled;
+  //     // const balance2 = await token.balanceOF(beneficiary1);
+  //     await factory.setDeposit(owner, amount, { from: beneficiary1}).should.be.fulfilled;
+  //     const balance2 = await token.balanceOF(beneficiary1);
+  //     assert.equal(balance1 - amount, balance2);
+  //   });
+  //   it("4-2 updateDepositValueAtNow", async () => {
+  //      await token.updateDepositValueAtNow(checkpoints, depositValue, claimerDepositValue, to {
+  //
+  //      }).should.be.fulfilled;
+  //    });
+  //   it("4-3 depositBalanceOfAt", async () => {
+  //     await token.depositBalanceOfAt(owner, blockNumber {
+  //
+  //     }).should.be.fulfilled;
+  //   });
+  //   it("4-4 getDepositValueAt", async () => {
+  //     await token.getDepositValueAt(checkpoints, block {
+  //
+  //     }).should.be.fulfiled
+  //   });
+  //   it("4-5 claimerBalanceAt", async () => {
+  //     await token.claimerBalanceAt(owner, blockNumber, to{
+  //
+  //     }).should.be.fulfiled
+  //   });
+  //   it("4-6 getClaimerValueAt", async () => {
+  //     await token.getClaimerValueAt(checkpoints, block, to{
+  //
+  //     }).should.be.fulfiled
+  //   });
+  //   it("4-7 withdrawDeposit", async () => {
+  //      const balance1 =  await token.balanceOf(beneficiary1);
+  //      console.log(balance1);
+  //
+  //      await factory.withdrawDeposit (, to, amount {}).should.be.fulfiled
+  //    });
+  // });
 
   // describe("#5 Auction", async () => {
   //   it("5-1 creat auction", async () => {
-  //     const balance1 = await token.balanceOf(msg.sender);
+  //     const balance1 = await token.balanceOf(beneficiary1);
   //     console.log(balance1);
-  //     await token.generateTokens(msg.sender, amount, { from: owner })
-  //       .should.be.fulfilled;
+  //     // await token.generateTokens(beneficiary1, amount, { from: owner })
+  //     //   .should.be.fulfilled;
   //
-  //     await token.createAuction(lowestprice, agentFee, auctionEndTime {
-  //     }).should.be.fulfilled;
-  //     const balcance2 = await token.balance
-  //     console.log(balance2);
-  //     assert.equal(balance2 - amount, balance1);
+  //     const lowestprice = ether(0.00001);
+  //     const auctionEndTime = 1829287689;
+  //     const agentFee = ether(0.000001);
+  //     console.log("lowestprice:" + lowestprice + " auctionEndTime:" + auctionEndTime + " agentFee:" + agentFee);
+  //     // await token.generateTokens(msg.sender, amount, { from: owner })
+  //     //   .should.be.fulfilled;
+  //     //No events were emitted
+  //     await token.createAuction(lowestprice, agentFee, auctionEndTime,
+  //       {from: beneficiary1, }).should.be.fulfilled;
+  //     //assert.notequal(auctionEndTime, NOW);
   //   });
-  //   it("5-2 Bid auction", async () => {
-  //     const balance = await token.balanceOf(beneficiary);
-  //     console.log(balance);
+  //   // it("5-2 Bid auction", async () => {
+  //   //   const balance1 = await token.balanceOf(other);
+  //   //   console.log(other);
+  //   //
+  //   //   await token.bidAuction(bid, {
+  //   //   keyTimeStamp: web3.timestamp,
+  //   //   from: other,
+  //   //   bid:2}).should.be.fulfilled;
+  //   //
+  //   //   const balance2 = await token.balanceOf(other);
+  //   //   console.log(balance2);
+  //   //   assert.equal(balance1 - bid, balance2);
+  //   // });
+  //   // it("5-3 escro auction", async () => {
+  //   //   const balance1 = await token.balanceOf(other);
+  //   //   console.log(balance1);
+  //   //   await token.escroAuction({
+  //   //     agentFee: 1,
+  //   //     from: other,
+  //   //   }).should.be.fulfilled;
+  //   //   const balance2 = await token.balancOf(other);
+  //   //   console.log(balance2);
+  //   //   assert.equal(balance);
+  //   // });
+  //   // it("5-3 withDraw auction", async () => {
+  //   //   const balance1 = await token.balanceOf(beneficiary1);
+  //   //   const balance2 = await token.balanceOf(other);
+  //   //   console.log(balance1);
+  //   //   console.log(balance2);
+  //   //   await token.escroAuction(beneficiary1, amount).should.be.fulfilled;
+  //   //
+  //   // });
+  // });
+  // describe("#6 lease", async () => {
+  //   it("7-1 create lease", async () => {
+  //     const balance1 = await token.balanceOf(beneficiary1);
   //
-  //     await token.generateTokens(beneficiary1, amount, { from: owner })
+  //     console.log(balance1);
+  //
+  //     await token.createlease(deposit, leaseFee,
+  //       useCA, agentFee, paymentTimestamp, { from: beneficiary1 })
   //       .should.be.fulfilled;
   //
-  //     await token.createAuction(lowestprice, agentFee, auctionEndTime{
-  //
-  //     });
+  //     const balance2 = await token.balanceOf(beneficiary1);
   //
   //     console.log(balance2);
-  //     assert.equal(balance2 - amount, balance1);
+  //
+  //     //assert.equal(balance2 - amount, balance1);
   //   });
-  //   it("5-3 creat auction", async () => {
-  //     const balance = await token.balanceOf(beneficiary);
-  //     console.log(balance);
+  //   it("7-2 apply sale", async () => {
   //
-  //     await token.generateTokens(beneficiary1, amount, { from: owner })
-  //       .should.be.fulfilled;
-  //
-  //     await token.createAuction(lowestprice, agentFee, auctionEndTime{
-  //
-  //     });
-  //
-  //     console.log(balance2);
-  //     assert.equal(balance2 - amount, balance1);
   //   });
-  //   it("5-4 creat auction", async () => {
-  //     const balance = await token.balanceOf(beneficiary);
-  //     console.log(balance);
+  //   it("7-3 confrim lease CA", async () => {
   //
-  //     await token.generateTokens(beneficiary1, amount, { from: owner })
-  //       .should.be.fulfilled;
+  //   });
+  //   it("7-4 withdraw lease fee", async () => {
   //
-  //     await token.createAuction(lowestprice, agentFee, auctionEndTime{
+  //   });
+  //   it("7-5 withdraw pre deposit lease ", async () => {
   //
-  //     });
-  //
-  //     console.log(balance2);
-  //     assert.equal(balance2 - amount, balance1);
   //   });
   // });
+  describe("#7 Sale", async () => {
+    console.log("test");
+    it("7-1 create sale", async () => {
+      const balance1 = await token.balanceOf(beneficiary1);
+      const deposit = ether(0.0000000001);
+      const agentFee = ether(0.00000000001);
+      console.log("balance1: " + balance1);
+
+      await token.createSale(deposit, false, agentFee, {from: beneficiary1 }).should.be.rejected;
+      console.log(now);
+    });
+    it("7-2 apply sale", async () => {
+      const keyTimeStamp = now;
+      await token.applySale(beneficiary1, keyTimeStamp, {from: other }).should.be.fulfilled;
+    });
+    it("7-3 confrim trade CA", async () => {
+      await token.confirmTradeByCA(beneficiary, keyTimeStamp, {from: other}).should.be.fulfilled;
+    });
+  });
 });
